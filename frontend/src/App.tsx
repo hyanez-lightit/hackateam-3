e93ac2d41ea094c75d753ddb35f5ceaa01f91d74
LeoBLightIt <leonardo.benveniste@lightit.io>
2023-11-30 17:50:02 -0300

    UI update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,6 +26,7 @@ const animation = `
 
 export const App = () => {
   const [msgAvailable, setMsgAvailable] = useState(false);
+  const [visibleInsights, toggleInsights] = useState(false);
   
   return (
     <div className="flex h-screen flex-col">
@@ -33,18 +34,32 @@ export const App = () => {
       <ToastContainer position="bottom-right" pauseOnFocusLoss={false} />
       <div className="w-full h-full relative" style={{background: "url('https://d33v4339jhl8k0.cloudfront.net/docs/assets/5bbe96a22c7d3a04dd5b8761/images/5ea88836042863474d19c944/file-pR2XT8TZj1.png')"}}>
       
-        <button onClick={() => {setMsgAvailable(!msgAvailable)}}
+        <div
           className={`fixed bottom-12 right-2 h-20 w-20 bg-blue-800 hover:bg-blue-700 rounded-full flex items-center justify-center text-white p-3.5 transition-all ${msgAvailable ? 'pulse' : 'hover:scale-105'}`}>
-          <div className='w-full h-full'>
+          <button className='w-full h-full' onClick={() => {
+              setMsgAvailable(!msgAvailable);
+              toggleInsights(false);
+            }}>
             <img src="/public/logo.svg" alt="" />
-          </div>
-          {msgAvailable && <div className="absolute bg-blue-800 rounded-lg shadow right-[75%] bottom-[75%] text-black w-fit pl-6">
-            <div className="bg-white h-full py-4 pl-4 pr-12 rounded-r-lg flex flex-col items-start gap-1">
-              <span className='text-[#35388C] text-xs font-semilight uppercase'>Recommendation</span>
-              <span className='text-[#4B5563] text-sm font-lg whitespace-nowrap'>Your patient may have Diabetes Type 2</span>
+          </button>
+          {msgAvailable && <div className="absolute bg-blue-800 rounded-lg shadow-xl right-[75%] bottom-[75%] text-black w-fit pl-6">
+            <div className="bg-white h-full py-4 pl-4 rounded-r-lg flex flex-col items-start gap-4">
+              <div className="flex flex-col pr-12 gap-1">
+                <span className='text-[#35388C] text-sm font-semibold uppercase tracking-wide'>Recommendation</span>
+                <span className='text-[#4B5563] font-lg whitespace-nowrap'>Your patient may have <span className='font-semibold'>Diabetes Type 2</span></span>
+                {visibleInsights && (
+                  <>
+                  <span className='text-[#35388C] text-sm font-semibold uppercase mt-4 tracking-wide'>Why</span>
+                  <span className='text-[#4B5563] font-lg whitespace-nowrap'>I found 4 patients with similar history that evolved into a <span className='font-semibold'>Diabetes Type 2</span></span>
+                  </>
+                )}
+              </div>
+              <div className="flex justify-end w-full pr-4">
+                <button className="text-sm font-medium text-[#35388C]" onClick={() => {toggleInsights(!visibleInsights)}}>{visibleInsights ? "Hide insights" : "View insights"}</button>
+              </div>
             </div>
           </div>}
-        </button>
+        </div>
       
       </div>
       <footer
